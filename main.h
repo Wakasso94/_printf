b383230d7921d4c690e94561b1fc1e506aa05bda
nair0lf32 <florianedem@gmail.com>
2022-04-22 19:02:44 +0100

    trying to fix undefined references

--- main.h
+++ main.h
@@ -6,25 +6,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
+
 void _putchar(char c, int fd);
 char *ft_itoa(int n);
 
-int	_printf(const char *format, ...);
+int	ft_print_ptr(unsigned long long ptr);
+void ft_put_ptr(uintptr_t num);
+int	ft_ptr_len(uintptr_t num);
+
 int	ft_formats(va_list args, const char format);
 int	ft_printchar(int c);
 int	ft_printstr(char *str);
-int	ft_print_ptr(unsigned long long ptr);
+
 int	ft_printnbr(int n);
 int	ft_print_unsigned(unsigned int n);
 int	ft_print_hex(unsigned int num, const char format);
 int	ft_printpercent(void);
 
-void ft_putstr(char *str);
-void ft_put_ptr(uintptr_t num);
-int	ft_ptr_len(uintptr_t num);
 char *ft_uitoa(unsigned int n);
 int	ft_num_len(unsigned	int num);
 void ft_put_hex(unsigned int num, const char format);
 int	ft_hex_len(unsigned	int num);
+
+void ft_putstr(char *str);
+int	_printf(const char *format, ...);
 
 #endif
